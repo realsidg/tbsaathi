0e330aea9ebe614de51372625612413fa7168fc2
realsidg <i@siddharthgarg.in>
2019-10-13 15:17:26 +0530

    predicted

--- predict.py
+++ predict.py
@@ -1,1 +1,9 @@
+import pickle
 import Orange
+
+def predict(age, gender, height, weight, EverSmoke,	Everdrink, Sputum,	CoughWeeks,	Fever,	Nightsweats,Weightloss,	Chestpain,	Hardbreath):
+    with open("model_orange_neural_network.pkcls", "rb") as f:
+        model = pickle.load(f)
+        test = [age, gender, height, weight, EverSmoke,	Everdrink, Sputum,	CoughWeeks,	Fever,	Nightsweats,Weightloss,	Chestpain,	Hardbreath]
+        prob = model(test, model.Probs)
+        print(prob[1])
