5717733a5aa5c433c90d18d7c580729a7c2984b4
realsidg <i@siddharthgarg.in>
2019-10-15 02:32:03 +0530

    Fixes

--- predict.py
+++ predict.py
@@ -1,9 +1,15 @@
 import pickle
 import Orange
+import sys
 
-def predict(age, gender, height, weight, EverSmoke,	Everdrink, Sputum,	CoughWeeks,	Fever,	Nightsweats,Weightloss,	Chestpain,	Hardbreath):
+def predict(val):
+    # print(val)
+    age ,sex,everSmoker,everDrinker,sputum,coughWeeks,fever,nightSweats,weightLoss,chestPain,hardBreath,height,weight = list(val.split(','))
     with open("model_orange_neural_network.pkcls", "rb") as f:
         model = pickle.load(f)
-        test = [age, gender, height, weight, EverSmoke,	Everdrink, Sputum,	CoughWeeks,	Fever,	Nightsweats,Weightloss,	Chestpain,	Hardbreath]
+        test = [age, sex, height, weight, everSmoker,	everDrinker, sputum,	coughWeeks,	fever,	nightSweats,weightLoss,	chestPain,	hardBreath]
         prob = model(test, model.Probs)
         print(prob[1])
+
+predict(sys.argv[1])
+# 50.39178082,	2,	168,	62,	0,	1,	0,	2,	0,	0,	1,	1,	1
